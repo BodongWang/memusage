9c0aff292148e7863e7ca66aff9ff2ff5bbd1b9e
Bodong Wang <bodong@mellanox.com>
2018-10-03 10:31:51 -0500

    Separate classes, check_rep works
    Signed-off-by: Bodong Wang <bodong@mellanox.com>

--- mem_check.py
+++ mem_check.py
@@ -5,13 +5,34 @@ from subprocess import call
 import socket
 import os.path
 
+def run_cmd(cmd, cleanup=None):
+	#args = shlex.split(cmd)
+	try:
+		output = subprocess.check_output(cmd, shell=True)
+	except subprocess.CalledProcessError as e:
+		print e.output
+		if cleanup is not None:
+			cleanup()
+		sys.exit("cmd: " + cmd)
+	return output
+
+def query_free_mem(unit="MB"):
+	cmd = "vmstat -s"
+	vmstat_output = run_cmd(cmd)
+	lines = vmstat_output.splitlines()
+	mem_used = lines[1].lstrip().split()[0]
+	mem = int(mem_used)
+	if unit is "MB":
+		return mem / 1024
+	else:
+		return mem
+
 class check_mem_usage():
 	def __init__(self, args=[]):
-		self.server = False
-		self.num_vf = 1
-		self.mlnx_dev = 'mlx5_x'
-		self.num_rules = 0
-		self.rules_script = "./tc-batch-l2-random-rule.sh"
+		mlnx_dev = "mlx5_dev"
+		num_vf = 0
+		server = False
+		num_rules = 0
 		try:
 			opts, args = \
 				getopt.getopt(args,"hi:n:s:r:",
@@ -21,35 +42,45 @@ class check_mem_usage():
 			sys.exit(1)
 		for opt, arg in opts:
 			if opt == '-h':
-				print 'mem_usage.py -i <interface> -n <num_vf> -s <0|1> -r <num_rules>'
+				print ('mem_usage.py -i <interface> -n <num_vf> \
+						-s <0|1> -r <num_rules>')
 				sys.exit()
 			elif opt in ("-i", "--interface"):
-				self.mlnx_dev = arg
+				mlnx_dev = arg
 			elif opt in ("-n", "--num_vf"):
-				self.num_vf = int(arg)
+				num_vf = int(arg)
 			elif opt in ("-s", "--server"):
-				self.server = bool(int(arg))
+				server = bool(int(arg))
 			elif opt in ("-r", "--num_rules"):
-				self.num_rules= int(arg)
+				num_rules= int(arg)
 
+		logging.basicConfig()
+		logger = logging.getLogger("mem_check")
+		logger.setLevel(logging.DEBUG)
+
+		if (num_rules > 0):
+			rules_test = check_rule_mem(logger, mlnx_dev, num_rules)
+			rules_test.run()
+		else:
+			rep_test = check_rep_mem(logger, mlnx_dev,server, num_vf)
+			rep_test.run()
+
+
+class check_rep_mem():
+	def __init__(self, log, mlx5_dev, server=True, num_vf=1):
+		self.logger= log
+		self.mlnx_dev = mlx5_dev
+		self.server = server
+		self.num_vf = num_vf
 		self.host = '192.168.100.2'
 		self.port = 5000
 		self.dmesg_file = "/tmp/page_usage"
 		self.page_file = "/tmp/give_page"
 
-		logging.basicConfig()
-		self.logger = logging.getLogger("mem_check")
-		self.logger.setLevel(logging.DEBUG)
-
-		if (self.num_rules == 0):
-			if (self.server):
-				self.server_init()
-			else:
-				self.client_init()
+		if (server):
+			self.server_init()
 		else:
-			if not os.path.exists(self.rules_script):
-				self.logger.error("%d doesn't exist", rules_script)
-				sys.exit(2)
+			self.client_init()
 
 	def find(self, substr, infile, outfile):
 		with open(infile) as a, open(outfile, 'w') as b:
@@ -65,65 +96,52 @@ class check_mem_usage():
 				count += int(words[words.index(substr) + 1].replace(",", ""))
 		return count
 
-	def end_connection(self):
+	def clean_up(self):
 		if (self.server):
 			self.server_conn.close()
+			self.stop_record_dmesg()
 		else:
 			self.client_socket.close()
 
-	def run_cmd(self, cmd):
-		#args = shlex.split(cmd)
-		try:
-			output = subprocess.check_output(cmd, shell=True)
-		except subprocess.CalledProcessError as e:
-			print e.output
-			if (self.num_rules == 0):
-				self.end_connection()
-			sys.exit("cmd: " + cmd)
-		return output
+	def check_rep_cmd(self, cmd):
+		return run_cmd(cmd, self.clean_up)
 
-	def query_free_mem(self):
-		cmd = "vmstat -s"
-		vmstat_output = self.run_cmd(cmd)
-		lines = vmstat_output.splitlines()
-		mem_used= lines[1].lstrip().split()[0]
-		return mem_used
-
-	def setup_vf(self):
-		cmd_current_vf = ("cat /sys/class/infiniband/" + self.mlnx_dev + "/device/mlx5_num_vfs")
-		cmd_reset_vf = ("echo 0 > /sys/class/infiniband/" + self.mlnx_dev + "/device/mlx5_num_vfs")
-		cmd_set_vf = ("echo "+ str(self.num_vf) + " > /sys/class/infiniband/" + self.mlnx_dev +
+	def client_setup_vf(self):
+		cmd_current_vf = ("cat /sys/class/infiniband/" + self.mlnx_dev +
+				"/device/mlx5_num_vfs")
+		cmd_reset_vf = ("echo 0 > /sys/class/infiniband/" +
+				self.mlnx_dev +
+				"/device/mlx5_num_vfs")
+		cmd_set_vf = ("echo "+ str(self.num_vf) +
+				" > /sys/class/infiniband/" + self.mlnx_dev +
 				"/device/mlx5_num_vfs")
 		message = 'done'
 
-		count = self.run_cmd(cmd_current_vf)
+		count = self.check_rep_cmd(cmd_current_vf)
 		self.logger.debug("%s has %d VF enabled", self.mlnx_dev, int(count))
 		if (int(count) > 0):
 			self.logger.debug("Reseting %s num of VF to 0", self.mlnx_dev)
-			self.run_cmd(cmd_reset_vf)
+			self.check_rep_cmd(cmd_reset_vf)
 			time.sleep(40)
 		self.client_socket.send(message.encode())
 
 		self.logger.debug("Setting %s num of VF to %d", self.mlnx_dev, self.num_vf)
-		self.run_cmd(cmd_set_vf)
+		self.check_rep_cmd(cmd_set_vf)
 		time.sleep(5)
 		self.client_socket.send(message.encode())
 
-	def setup_rules(self):
-		cmd_setup_rules = (self.rules_script + " " + self.mlnx_dev + " " + str(self.num_rules))
-		self.run_cmd(cmd_setup_rules)
-		time.sleep(3)
-
 	def start_record_dmesg(self):
-		self.run_cmd("echo 'module mlx5_core +p' > /sys/kernel/debug/dynamic_debug/control")
-		self.run_cmd("dmesg -C")
+		self.check_rep_cmd("echo 'module mlx5_core +p' > \
+			/sys/kernel/debug/dynamic_debug/control")
+		self.check_rep_cmd("dmesg -C")
 		cmd = ("dmesg -w > " + self.dmesg_file + " &")
 		self.logger.debug("Saving dmesg to %s", self.dmesg_file)
-		self.run_cmd(cmd)
+		self.check_rep_cmd(cmd)
 
 	def stop_record_dmesg(self):
-		self.run_cmd("killall dmesg")
-		self.run_cmd("echo 'module mlx5_core -p' > /sys/kernel/debug/dynamic_debug/control")
+		self.check_rep_cmd("killall dmesg")
+		self.check_rep_cmd("echo 'module mlx5_core -p' > \
+			/sys/kernel/debug/dynamic_debug/control")
 
 	def server_init(self):
 		server_socket = socket.socket()
@@ -141,7 +159,7 @@ class check_mem_usage():
 			    break
 
 	def server_bringup_rep(self):
-		self.run_cmd("ifconfig rep0-1 up")
+		self.check_rep_cmd("ifconfig rep0-1 up")
 		time.sleep(3)
 		self.logger.info("Bringup rep0-1")
 
@@ -155,11 +173,11 @@ class check_mem_usage():
 			# Wait for host VF to reset
 			self.server_wait_for_vf()
 			self.start_record_dmesg()
-			mem_before = int(self.query_free_mem()) / 1024
+			mem_before = query_free_mem()
 			# Wait for host VF to setup
 			self.server_wait_for_vf()
 			self.server_bringup_rep()
-			mem_after = int(self.query_free_mem()) / 1024
+			mem_after = query_free_mem()
 			self.stop_record_dmesg()
 			self.server_conn.close()
 
@@ -174,15 +192,33 @@ class check_mem_usage():
 			self.logger.info("\tPage Usage Total:\t\t%d", page_used)
 			self.logger.info("-----------------------------------------")
 		else:
-			self.setup_vf()
+			self.client_setup_vf()
 			self.client_socket.close()
 			self.logger.info("Set up %d VFs done.", self.num_vf)
 
+	def run(self):
+		self.check_rep()
+
+class check_rule_mem():
+	def __init__(self, log, rep, num_rules=1):
+		self.rules_script = "./tc-batch-l2-random-rule.sh"
+		self.rep = rep
+		self.num_rules = num_rules
+		self.logger = log
+		if not os.path.exists(self.rules_script):
+			self.logger.error("%d doesn't exist", rules_script)
+			sys.exit(2)
+
+	def setup_rules(self):
+		cmd_setup_rules = (self.rules_script + " " + self.rep+ " " + str(self.num_rules))
+		run_cmd(cmd_setup_rules)
+		time.sleep(3)
+
 	def check_rules(self):
 		self.logger.info("Check mem usage of ovs rules")
-		mem_before = int(self.query_free_mem()) / 1024
+		mem_before = query_free_mem()
 		self.setup_rules()
-		mem_after = int(self.query_free_mem()) / 1024
+		mem_after = query_free_mem()
 		mem_used = abs(mem_after - mem_before)
 
 		self.logger.info("-----------------------------------------")
@@ -192,11 +228,8 @@ class check_mem_usage():
 		self.logger.info("-----------------------------------------")
 
 	def run(self):
-		if (self.num_rules):
-			self.check_rules()
-		else:
-			self.check_rep()
+		self.check_rules()
 
 if __name__ == "__main__":
 	test = check_mem_usage(sys.argv[1:])
-	test.run()
+	exit()
